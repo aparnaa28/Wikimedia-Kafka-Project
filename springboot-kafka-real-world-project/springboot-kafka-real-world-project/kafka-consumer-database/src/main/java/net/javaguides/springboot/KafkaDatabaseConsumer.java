cd6c9376fcda05017304a96deb0986ad046ba67b
aparnaa28 <49586727+aparnaa28@users.noreply.github.com>
2022-10-06 11:25:15 +0530

    Add files via upload

---
+++ springboot-kafka-real-world-project/springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.entity.WikimediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    private WikimediaDataRepository wikimediaDataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.wikimediaDataRepository = dataRepository;
+    }
+
+    @KafkaListener(topics="wikimedia_recent_change", groupId = "myGroup")
+    public void consume(String eventMessage)
+    {
+        LOGGER.info(String.format("event message received-> %s", eventMessage));
+        WikimediaData wikimediaData=new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
+    }
+}
